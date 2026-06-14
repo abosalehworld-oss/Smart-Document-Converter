2ef58c538e44de71a2f15f8b7441de86f08e7d8f
SDC-Project <project@secure.local>
2026-06-14 17:42:03 +0300

    chore: clean up deleted test and temp scripts

--- create_shortcut.ps1
+++
@@ -1,7 +0,0 @@
- = New-Object -ComObject WScript.Shell  
- = 'H:\بروجكت برنامج OCR\dist\Portable_SmartDocConverter\Smart Document Converter.lnk'  
- = .CreateShortcut()  
-.TargetPath = 'H:\بروجكت برنامج OCR\dist\Portable_SmartDocConverter\Run_Program.bat'  
-.WorkingDirectory = 'H:\بروجكت برنامج OCR\dist\Portable_SmartDocConverter'  
-.IconLocation = 'H:\بروجكت برنامج OCR\dist\Portable_SmartDocConverter\app\assets\icon.ico'  
-.Save()  
